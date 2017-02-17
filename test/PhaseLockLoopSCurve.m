0b89766c0ff2f80ac254f8bc252c658145a3c2eb
w395650200 <dawei.zju@gmail.com>
2017-02-18 06:28:05 +0800

    rewrite the PhaseLockLoopScurve; Two observations are made: mean error and instantaneous error, seperately;

--- test/PhaseLockLoopSCurve.m
+++ test/PhaseLockLoopSCurve.m
@@ -1,5 +1,5 @@
 %% TEST SCRIPT FOR PHASE-LOCKED LOOP S-CURVE
-%
+%Observe the PLL S-Curve under different SNR
 %
 %% QPSK WITH TIME VARYING PHASE ERROR
 clear
@@ -19,8 +19,8 @@ symTx = symbolizerGrayQam(bitTx);
 symRef = symTx;
 
 
-%% High SNR Phase-locked loop S-curve
-snr = 20:20;
+%% Observe averaged Phase-locked loop S-curve for various SNR
+snr = -20:20;
 
 % define phase error
 phaseNoise = -pi:0.1:pi+0.1;
@@ -41,36 +41,12 @@ for ii = 1:length(snr)
     end
 end
 
-figure; plot(phaseNoise/pi,sc); grid on; xlim([-1 1]);
+h1=figure; plot(phaseNoise/pi,sc); grid on; xlim([-1 1]);
 xlabel('Phase Error (\phi-\phi_k)/\pi'); ylabel('PED mean');
 
 sc = [];
 
-
-%% Low SNR Phase-locked loop S-curve
-snr = -10:0;
-
-for ii = 1:length(snr)
-    sigma2 = 10*log10(sp) - snr(ii);
-    z = wgn(1,symlen,sigma2,'dbw','complex');
-    for k = 1:length(phaseNoise)
-        % data model
-        symTxPn = symTx.*exp(1j*phaseNoise(k))+z;
-        % average over random data; normalized wrt the signal power
-        sc(k,ii) = (1/sp)*mean(imag(symTxPn.*conj(symRef)));
-    end
-end
-
-figure; plot(phaseNoise,sc); grid on; xlim([-pi pi]);
-xlabel('Phase Error'); ylabel('PED mean');
-
-% the averaged ped output forms sinusoidal s-curve from -pi to pi, and
-% holds for both high and low SNR
-
-sc = [];
-
-
-%% Low SNR Phase-locked loop S-curve instantanuous
+%% Observe instantanuous Phase-locked loop S-curve for low SNR
 snr = -10;
 
 for ii = 1:10
@@ -91,12 +67,13 @@ for ii = 1:10
     end
 end
 
-figure; plot(phaseNoise,sc); grid on; xlim([-pi pi]);
+h2=figure; plot(phaseNoise,sc); grid on; xlim([-pi pi]);
 xlabel('Phase Error'); ylabel('PED');
 
 % the instanstanuous gradient gets noisier for lower snr and requires
 % smaller stepsize (smaller loop filter bandwidth) to lower the steady
 % state error
+mngFigureWindow(h1,h2);
 
 
 % EOF
