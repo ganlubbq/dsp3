6fce62e8058cf74a70e1bd3b269ce80b55fb2a30
w395650200 <dawei.zju@gmail.com>
2017-02-18 06:15:03 +0800

    add display of PDF and Likelihood and compare them; using gaussian distribution

--- test/ClassicalEstimationPDF.m
+++ test/ClassicalEstimationPDF.m
@@ -1,5 +1,5 @@
 %% TEST SCRIPT FOR PARAMETERIZED PDF OF CLASSICAL ESTIMATION
-% SLICE ALONG FIXED DATA WOULD GENERATE A LIKELIHOOD OF UNKNOWN PARAMTER
+% SLICE ALONG FIXED DATA WOULD GENERATE A LIKELIHOOD FUNCTION OF UNKNOWN PARAMTER
 %
 % Kay, Steven M. "Fundamentals of statistical signal processing: estimation
 % theory." (1993).
@@ -7,22 +7,32 @@
 %% Unknown parameter is deterministic
 clear
 
-% unknown parameter
+% nominal data
+x = -1:0.05:1;
+
+% the deterministic unknown parameter, gaussian mean
 theta = -1:0.05:1;
 
-% likelihood function
-PDF = 1/sqrt(2*pi*0.1)*exp(-theta.^2/(2*0.1));
-PDF = PDF';
+% % 
+% PDF = 1/sqrt(2*pi*0.1)*exp(-(x.-theta).^2/(2*0.1));
+% PDF = PDF';
 
 % varying with data
 PDF3 = [];
 for jj = 1:length(theta)
-    PDF3 = [PDF3 PDF+0.2*rand(length(theta),1)];
+    PDF = 1/sqrt(2*pi*0.1)*exp(-(x-theta(jj)).^2/(2*0.1));
+    PDF = PDF';
+    PDF3 = [PDF3 PDF];
 end
 
-% nominal data
-x = -1:0.05:1;
-
-figure;
-mesh(theta,x,PDF3); view([-134 28]);
+% plot the parameterized pdf
+h1=figure;
+mesh(theta,x,PDF3); view([-64 19]);
 xlabel('x'); ylabel('\theta'); zlabel('PDF');
+
+% the likelihood function can be obtained by fix data x and varying unknown
+% parameter
+h2=figure;
+plot(theta,PDF3(10,:)); grid on
+
+mngFigureWindow(h1,h2);
