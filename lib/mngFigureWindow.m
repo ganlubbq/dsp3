56f24354e0045c07c9e3be2032cd131080605855
w395650200 <dawei.zju@gmail.com>
2017-02-18 06:13:36 +0800

    remove the up-down mode in mngFigureWiindow, keep it simple

--- lib/mngFigureWindow.m
+++ lib/mngFigureWindow.m
@@ -1,36 +1,23 @@
-function mngFigureWindow( h1, h2, config )
-% Manage position of 2 figures in the mode configured by input
+function mngFigureWindow( h1, h2)
+% Manage position of 2 figures in the mode configured left-right side by
+% side
 % 
-% Example: mngFigureWindow( h1, h2, config )
-% 
-% Input: config - lr or ud
-% 
-% Reference: 
-% 
-% Note: 
+% Example: mngFigureWindow( h1, h2, config)
 % 
 % See Also: 
 
-if nargin < 3
-    config = 'lr';
-end
-
 scrsz = get(0,'ScreenSize');
+
+screen_width = scrsz(3);
+screen_height = scrsz(4);
 
 p1 = get(h1,'Position');
 p2 = get(h2,'Position');
 
-if strcmpi(config,'lr')
-    p1(1) = 100;
-    p2(1) = p1(1) + p1(3) + 30;
-    set(h1,'Position',p1)
-    set(h2,'Position',p2)
-elseif strcmpi(config,'ud')
-    p1(2) = scrsz(4) - p1(4) - 80;
-    p2(2) = p1(2) - p2(4) - 80;
-    set(h1,'Position',p1)
-    set(h2,'Position',p2)
-end
+p1(1) = round(screen_width/2)-p1(3);
+p2(1) = p1(1) + p1(3) + 10;
+set(h1,'Position',p1)
+set(h2,'Position',p2)
 
 drawnow;
 
