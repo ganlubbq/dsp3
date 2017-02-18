132a63146e5e22f590aeb4c0e914c4710856456a
w395650200 <dawei.zju@gmail.com>
2017-02-18 19:03:20 +0800

    add input parameter check to calcSnrBerPenalty.m; checking column and scalar input

--- lib/calcSnrBerPenalty.m
+++ lib/calcSnrBerPenalty.m
@@ -1,10 +1,22 @@
-function [ penalty ] = calcSnrBerPenalty( SNR, BER1, BER2, tarBER )
+function [ penalty] = calcSnrBerPenalty( SNR, BER1, BER2, tarBER)
 %UNTITLED Summary of this function goes here
 %   Detailed explanation goes here
 
 % everything must be in column and log10
+if ~iscolumn(SNR)
+    warning('input snr has to be a column vector'); keyboard;
+end
+if ~iscolumn(BER1)
+    warning('input ber1 has to be a column vector'); keyboard;
+end
+if ~iscolumn(BER2)
+    warning('input ber2 has to be a column vector'); keyboard;
+end
+if ~isscalar(tarBER)
+    warning('input tarBER has to be a scalar'); keyboard;
+end
 
-% set the resolution to be 0,01dB
+% set the resolution to be 0,01 dB
 snrFine = linspace(SNR(1),SNR(end),(SNR(end)-SNR(1))/0.01+1);
 
 ber1Fine = interp1(SNR,BER1,snrFine(:));
