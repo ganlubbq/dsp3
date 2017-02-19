3b9de812e9003b14df41b1c42fa99f211a66d74e
w395650200 <dawei.zju@gmail.com>
2017-02-20 05:49:39 +0800

    nothing has been done

--- lib/estimateCarrierPhaseML.m
+++ lib/estimateCarrierPhaseML.m
@@ -1,15 +1,17 @@
-function theta = estimateCarrierPhaseML(observations,blocksize,mn)
+function theta = estimateCarrierPhaseML(observations, blocksize, mn)
 %ESTIMATECARRIERPHASEML Implementing the carrier phase estimation based on
 %the decision directed ML criterial. The algorithm works blockwise, within
 %each block the phase is assumed to be deterministic and constant. 
 %
 % mn in the input stands for mn-QAM
 
+
+
 % based on decision
 signals = slicer(observations,mn);
 
 % sliding window smoothing
-H = smooth(observations(:).*conj(signals),blocksize);
+H = smooth(observations(:) .* conj(signals), blocksize);
 
 % the use of angle func. limits the estimation range
 theta = angle(H);
