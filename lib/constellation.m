30e6f5d31937dd46d968229f78c5ebb9ae23ff02
w395650200 <dawei.zju@gmail.com>
2017-02-20 06:08:45 +0800

    rewrite constellation qam

--- lib/constellation.m
+++ lib/constellation.m
@@ -2,14 +2,38 @@ function c = constellation(mn)
 %CONSTELLATION Give the canonical form of uncoded constellations for mn-QAM
 %
 
-h = modem.qammod('M',mn);
 
-cr = h.Constellation;
-
-if mn == 8
-    c = [-2, -2j, 2j, 2, 3+3j, 3-3j, -3-3j, -3+3j];
-else
-    c = cr(:);
+switch mn
+    
+    case 2
+        c = [-1; 1];
+        
+    case 4
+        c = [-1+1j,-1-1j,1+1j,1-1j;];
+        
+    case 8
+        c = [-2, -2j, 2j, 2, 3+3j, 3-3j, -3-3j, -3+3j];
+        
+    case 16
+        ci = [-3 -3 -3 -3 -1 -1 -1 -1 1 1 1 1 3 3 3 3];
+        cq = [3 1 -1 -3 3 1 -1 -3 3 1 -1 -3 3 1 -1 -3];
+        c = ci + 1j*cq;
+        
+    case 64
+        ci = [-7*ones(1,8) -5*ones(1,8) -3*ones(1,8) -1*ones(1,8) ones(1,8) 3*ones(1,8) 5*ones(1,8) 7*ones(1,8)];
+        temp = [7 5 3 1 -1 -3 -5 -7];
+        cq = [temp temp temp temp temp temp temp temp];
+        c = ci + 1j*cq;
+        
+    case 256
+        ci = [-15*ones(1,16) -13*ones(1,16) -11*ones(1,16) -9*ones(1,16) -7*ones(1,16) -5*ones(1,16) -3*ones(1,16) -1*ones(1,16) 1*ones(1,16) 3*ones(1,16) 5*ones(1,16) 7*ones(1,16) 9*ones(1,16) 11*ones(1,16) 13*ones(1,16) 15*ones(1,16)];
+        temp = 15:-2:-15;
+        cq = [temp temp temp temp temp temp temp temp temp temp temp temp temp temp temp temp];
+        c = ci + 1j*cq;
+        
+    otherwise
+        warning('unsupported modulation format'); keyboard;
+        
 end
 
 return
