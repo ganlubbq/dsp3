060c3f89b6ed2a121db5e810cc3cb179726c89f0
w395650200 <dawei.zju@gmail.com>
2017-02-20 06:20:28 +0800

    replace slicer with makeHardDecision

--- lib/estimateCarrierPhaseML_hardDecision.m
+++ lib/estimateCarrierPhaseML_hardDecision.m
@@ -1,4 +1,4 @@
-function theta = estimateCarrierPhaseML(observations, blocksize, mn)
+function theta = estimateCarrierPhaseML_hardDecision(observations, blocksize, mn)
 %ESTIMATECARRIERPHASEML Implementing the carrier phase estimation based on
 %the decision directed ML criterial. The algorithm works blockwise, within
 %each block the phase is assumed to be deterministic and constant. 
@@ -8,78 +8,13 @@ function theta = estimateCarrierPhaseML(observations, blocksize, mn)
 
 
 % based on decision
-signals = slicer(observations,mn);
+signals = makeHardDecision(observations, mn);
 
 % sliding window smoothing
 H = smooth(observations(:) .* conj(signals), blocksize);
 
 % the use of angle func. limits the estimation range
 theta = angle(H);
-
-return
-
-% slice columns of complex input into symbols 
-function d = slicer(x,mn)
-
-xnorm = normalizeQam(x(:),mn);
-
-switch mn % MSB first
-	
-	case 4
-		bit = [msign(real(xnorm)), ~msign(imag(xnorm))];
-		
-	case 16
-		bound = 2;
-		bit = [msign(real(xnorm)), msign(real(xnorm)-ksign(real(xnorm))*bound),...
-			~msign(imag(xnorm)), ~msign(imag(xnorm)-ksign(imag(xnorm))*bound)];
-		
-	case 64 % 64qam slicing is combination of qpsk and 16qam, i.e.
-		% [4qam 16qam 16qam 4qam 16qam 16qam]
-		bound = 2;
-		
-		% 4qam header
-		bit1 = [msign(real(xnorm)), ~msign(imag(xnorm))];
-		
-		% convert 64qam symbol in one quater to 16qam
-		x = x-(ksign(real(xnorm))*4+ksign(imag(xnorm))*4j);
-		
-		% slicing 16qam
-		bit2 = [msign(real(xnorm)), msign(real(xnorm)-ksign(real(xnorm))*bound),...
-			~msign(imag(xnorm)), ~msign(imag(xnorm)-ksign(imag(xnorm))*bound)];
-		
-		bit = [bit1(:,1) bit2(:,1:2) bit1(:,2) bit2(:,3:4)];
-		
-	case 256
-		% TBA
-	otherwise
-		error('unsupported format');
-end
-
-% convert bit in rows to dec by MSB to LSB order
-twos = 2.^(log2(mn)-1:-1:0);
-twos = ones(length(xnorm),1)*twos;
-
-% index of symbols from topleft to bottomright by columns
-ndx = sum(bit.*twos,2)+1;
-
-% symbols from topleft to bottomright by columns
-c = constellation(mn);
-
-d = c(ndx);
-
-return
-
-% return 1 if x>0 and 0 if x<=0
-function s = msign(x)
-
-s = (x>0);
-
-return
-
-% return 1 if x>0 and -1 if x<=0
-function s = ksign(x)
-
-s = (x>0)*2-1;
 
 return
 
