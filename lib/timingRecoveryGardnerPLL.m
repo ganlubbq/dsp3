d8a18d506fa3464378e48fa4a5e42de1275d988e
w395650200 <dawei.zju@gmail.com>
2017-02-20 06:32:25 +0800

    renaming files

--- lib/timingRecoveryGardnerPLL.m
+++ lib/timingRecoveryGardnerPLL.m
@@ -10,7 +10,7 @@
 % 
 % See Also: 
 % 
-% Copyright 2015 default
+% Copyright 2015 Default
 
 function [symRtm, phEst]= GardnerPLL(symIn, sps, gain, int_method)
 
@@ -44,28 +44,35 @@ for jj = 1 : Ncol
     buffer( 1+N : numBuf-N-N ) = symIn(:,jj);
     
     if strcmp(int_method, 'linear')
+        
         for k = 1 : numSym
+            
             % Sampling instants for tau calculation
             fltIdx1 = N*( (k-1)*T  + tau(k));
             fltIdx2 = N*( k*T      + tau(k+1));
             fltIdx3 = N*( k*T - Ts + tau(k));
+            
             % Select the second symbol as the current symbol so as to have...
             % a buffer of symbols on both sides [-T..T..+2T]
             idx1_dn = N+1 + floor(fltIdx1);
             idx2_dn = N+1 + floor(fltIdx2);
             idx3_dn = N+1 + floor(fltIdx3);
+            
             % interpolate between 2 available points
             a1 = linearInterp(buffer, idx1_dn, fltIdx1);
             a2 = linearInterp(buffer, idx2_dn, fltIdx2);
             a3 = linearInterp(buffer, idx3_dn, fltIdx3);
+            
             % Timing error calculation
             b1 = real(a1 - a2) * real(a3);
             b2 = imag(a1 - a2) * imag(a3);
             eK = b1 + b2;
+            
             % Apply current estimate to next. Actrually we need k and k+1..
             % errors to calculate current error and output current symbol...
             % so k+2 error should be updated before we continue next iteration
             tau(k+2) = tau(k+1) + gain * eK;
+            
             if abs(tau(k+2)) > Ts
                 tau(k+2) = rem(tau(k+2) + Ts, T); % [-T, T], due to fmod */
                 if tau(k+2) < 0
@@ -74,6 +81,7 @@ for jj = 1 : Ncol
                 tau(k+2) = tau(k+2)-Ts;       % [-T/2, T/2] */
             end
         end
+        
         for k = 1 : numSym
             % always delay so tau must > zero
             if tau(k) < 0
@@ -81,30 +89,38 @@ for jj = 1 : Ncol
             else
                 tauInt = tau(k);
             end
+            
             % output symbol phase estimate
             phEst(k,jj) = tauInt * N;
             dn = k*N + 1 + floor(phEst(k,jj));
             symRtm(k,jj) = linearInterp(buffer, dn, phEst(k,jj));
         end
+        
     elseif strcmp(int_method, 'parabolic')
+        
         for k = 1 : numSym
+            
             % Sampling instants for tau calculation
             fltIdx1 = N*( (k-1)*T  + tau(k));
             fltIdx2 = N*( k*T      + tau(k+1));
             fltIdx3 = N*( k*T - Ts + tau(k));
+            
             % Select the second symbol as the current symbol so as to have...
             % a buffer of symbols on both sides [-T..T..+2T]
             idx1_dn = N+1 + floor(fltIdx1);
             idx2_dn = N+1 + floor(fltIdx2);
             idx3_dn = N+1 + floor(fltIdx3);
+            
             % interpolate between 2 available points
             a1 = ParabolicInterp(buffer, idx1_dn, fltIdx1);
             a2 = ParabolicInterp(buffer, idx2_dn, fltIdx2);
             a3 = ParabolicInterp(buffer, idx3_dn, fltIdx3);
+            
             % Timing error calculation
             b1 = real(a1 - a2) * real(a3);
             b2 = imag(a1 - a2) * imag(a3);
             eK = b1 + b2;
+            
             % Apply current estimate to next. Actrually we need k and k+1..
             % errors to calculate current error and output current symbol...
             % so k+2 error should be updated before we continue next iteration
@@ -117,6 +133,7 @@ for jj = 1 : Ncol
                 tau(k+2) = tau(k+2)-Ts;       % [-T/2, T/2] */
             end
         end
+        
         for k = 1 : numSym
             % always delay so tau must > zero
             if tau(k) < 0
