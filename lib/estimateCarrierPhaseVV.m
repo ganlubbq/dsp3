7d9e4e982dd87854b17bd7d438b6714813891318
w395650200 <dawei.zju@gmail.com>
2017-02-20 05:41:39 +0800

    clear help; add input check; more spaces

--- lib/estimateCarrierPhaseVV.m
+++ lib/estimateCarrierPhaseVV.m
@@ -1,35 +1,51 @@
-function [theta] = estimateCarrierPhaseVV(x,block_size,avg_mode)
+function [theta] = estimateCarrierPhaseVV(x, block_size, avg_mode)
 % Feedforward carrier phase estimation using VVPE.
 %
-%			avg_mode	- 0 for block 1 for sliding window
+% Input:
+%       avg_mode: 0 for block-wise; 
+%                 1 for sliding window
 %   
+% Note: VVPE suffers from cycle slip problem
+%
 % See also:
 
-% raise the phase to 4th power not the amplitude
-x4 = abs(x).*exp( 1j*angle( x.^ 4 ) );
 
-if avg_mode==0
+if ~iscolumn(x)
+    warning('first input has to be a column vector'); keyboard;
+end
+
+% raise the phase to 4th power not the amplitude
+x4 = abs(x) .* exp(1j * angle(x .^ 4));
+
+if avg_mode == 0
+    
 	% make sure length of x4 can be divided by block size
-	tmp = mod(length(x4),block_size);
+	tmp = mod(length(x4), block_size);
 	if tmp
-		x4 = [x4(:);zeros(tmp,1)];
-	end
-	x4b = reshape(x4(:),block_size,[]);
-	x4s = sum(x4b)/block_size;	
+		x4 = [x4; zeros(tmp,1)];
+    end
+    
+    % divided into blocks
+	x4b = reshape(x4(:), block_size, []);
+    
+	x4s = sum(x4b) / block_size;	
+    
 elseif avg_mode==1
-	% sliding average
-	x4s = smooth(x4(:),block_size);
+	
+    % sliding average
+	x4s = smooth(x4, block_size);
+
 else
-	error('incorrect mode number');
+	error('average mode not supported');
 end
 
 % unwrapping
-theta = unwrap(angle(x4s))/4;
+theta = unwrap(angle(x4s)) / 4;
 
-if avg_mode==0
-	theta = theta(:)*ones(1,block_size);
+if avg_mode == 0
+	theta = theta(:) * ones(1,block_size);
 	theta = theta.';
-	theta = theta(1:end);
+	theta = theta(:);
 end
 
 return
