ba429ad684bacb5cd716acc4bc182ff083b7adb5
w395650200 <dawei.zju@gmail.com>
2017-02-20 05:25:27 +0800

    refine comments and help info

--- lib/estimatorLeastSquaresBatch.m
+++ lib/estimatorLeastSquaresBatch.m
@@ -1,6 +1,11 @@
-function [ estimated ] = estimatorLeastSquaresBatch( signals, observations )
+function estimated = estimatorLeastSquaresBatch(signals, observations)
 %ESTIMATORLEASTSQUARESBATCH Implementing the least squares estimator based
-%on a linear mode.
+%on a linear model.
+% 
+% Linear model: signals * parameter_to_be_estimated = observations
+%
+%       H * \theta = x
+%
 % Signals in the input is denoted by H and is a Nxp matrix. Observations is
 % denoted by x and is a Nx1 vector. The estimation of model parameters is
 % given by (H^T H)^-1 * H^T x and is a px1 vector.
@@ -8,7 +13,7 @@ function [ estimated ] = estimatorLeastSquaresBatch( signals, observations )
 % Generally, N is required to be greater than or equal to p to form an
 % overdetermined system described by N linear equations with p unknowns.
 
-if size(signals,1)<size(signals,2)
+if size(signals,1) < size(signals,2)
     warning('underdetermined system');
 end
 
