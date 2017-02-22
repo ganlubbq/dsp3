b6d2a8bb79c7c65a1a70b01bc2ef56216eac2069
w395650200 <dawei.zju@gmail.com>
2017-02-23 04:48:38 +0800

    clean up

--- lib/calcRCFreqResponse.m
+++ lib/calcRCFreqResponse.m
@@ -10,26 +10,26 @@
 % 
 % See Also: 
 % 
-% Copyright Default
+% Copyright DEFAULT
 
-function H = calcRcosResponse(nSample,fs,fbaud,alpha,mode)
+function H = calcRCFreqResponse(nSample, fs, fbaud, alpha, mode)
 
-if nargin<5
+if nargin < 5
     mode = 0;
 end
 
-freq = [0:nSample/2]'/(nSample/2)*fs*0.5;
+freq = [0:nSample/2]' / (nSample/2) * fs * 0.5;
 
 H = zeros(size(freq));
-f_low = (1-alpha)*fbaud/2;
-f_high = (1+alpha)*fbaud/2;
+f_low = (1-alpha) * fbaud/2;
+f_high = (1+alpha) * fbaud/2;
 
-fndxl = find(freq<=f_low);
-fndxm = find(freq>f_low & freq<=f_high);
+fndxl = find(freq <= f_low);
+fndxm = find(freq > f_low & freq <= f_high);
 fndxh = [fndxl;fndxm];
 
 H(fndxl) = ones(size(fndxl));
-H(fndxm) = 1/2*(1+cos(pi/alpha*(freq(fndxm)/fbaud-(1-alpha)/2)));
+H(fndxm) = 0.5 * (1+cos(pi/alpha*(freq(fndxm)/fbaud - (1-alpha)/2)));
 
 % for ii = 1:length(f)
 %     if abs(f(ii)) <= (1-beta)/(2*T)
@@ -50,7 +50,7 @@ switch mode
         error('raised cosine configuration not supported');
 end
 
-H = [H;conj(flipud(H(2:end-1)))];
+H = [H; conj(flipud(H(2:end-1)))];
 
 return
 
