a0aaf5e68c21c6a9164b30b4bc73db20ddcadc20
w395650200 <dawei.zju@gmail.com>
2017-02-24 05:27:50 +0800

    Major update of spectrum analyzer

--- lib/spectrumAnalyzer.m
+++ lib/spectrumAnalyzer.m
@@ -1,7 +1,24 @@
-function [PSD, freqVect] = spectrumAnalyzer(x, freqVect)
+% According to parseval's theorem, the signal power in time domain equals
+% the signal power in frequency domain. However, due to normalization used
+% in Matlab, the power integrated in frequency domain via fft should be
+% scaled by 1/N
+%
+%   power_in_time = sum(abs(fft(signal)).^2) / N = power_in_frequency
+%
+% Therefore, the power density displayed on spectrum analyzer should be
+%
+%   psd_on_sa = abs(fft(signal)).^2 / N
+% 
+% so that the integration of it would be the total power
+%
+%   total_power = sum(psd_on_sa)
+%
+% However, the psd is not power per Hz, rather power per frequency slot of
+% spectrum analyzer, varying with the frequency resolution
 %
 % Copyright DEFAULT
 %
+function [PSD, freqVect] = spectrumAnalyzer(x, freqVect)
 
 if nargin < 2
 	nSamples = length(x);
@@ -10,7 +27,10 @@ else
     nSamples = length(x);
 end
 
-PSD = abs(fft(x) ./ nSamples) .^ 2;
+freqResolution = (max(freqVect) - min(freqVect)) / (nSamples - 1);
+
+% power in one freq slot
+PSD = abs(fft(x)) .^ 2 / nSamples;
 
 figure(); grid on
 plot(fftshift(freqVect), 10*log10(fftshift(PSD))); 
@@ -21,7 +41,8 @@ xlim([min(freqVect), max(freqVect)]);
 % to do
 
 xlabel('Frequency (Hz)'); 
-ylabel('PSD (dB/Hz)');
+ylabel('PSD (dB)');
+legend(sprintf('Frequency Resolution %.2f MHz', freqResolution/1e6));
 
 return
 
