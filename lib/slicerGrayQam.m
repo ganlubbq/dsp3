525c543d7f340b8ea63788f90a12448b15f8ed6e
w395650200 <dawei.zju@gmail.com>
2017-02-25 02:33:57 +0800

    delete old slicer

--- lib/slicerGrayQam.m
+++ lib/slicerGrayQam.m
@@ -12,6 +12,15 @@ function bit = slicerGrayQam(sym, M)
 % 
 % Reference: 
 % 
+% h = modem.qamdemod;
+% h.M = mn;
+% h.PhaseOffset = 0;
+% h.SymbolOrder = 'binary';
+% 
+% h.OutputType = 'integer';
+% bint = h.demodulate(x);
+% b = h.Constellation(bint+1);
+% 
 % Note: 
 % 
 % See Also: 
