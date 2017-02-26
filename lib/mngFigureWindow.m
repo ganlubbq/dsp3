ae81f8061909e2734bcb19932763be73e2575843
w395650200 <dawei.zju@gmail.com>
2017-02-26 21:20:17 +0800

    small change

--- lib/mngFigureWindow.m
+++ lib/mngFigureWindow.m
@@ -1,8 +1,8 @@
-function mngFigureWindow( h1, h2)
+function mngFigureWindow(h1, h2)
 % Manage position of 2 figures in the mode configured left-right side by
 % side
 % 
-% Example: mngFigureWindow( h1, h2, config)
+% Example: mngFigureWindow(h1, h2, config)
 % 
 % See Also: 
 
@@ -14,7 +14,7 @@ screen_height = scrsz(4);
 p1 = get(h1,'Position');
 p2 = get(h2,'Position');
 
-p1(1) = round(screen_width/2)-p1(3);
+p1(1) = round(screen_width/2) - p1(3);
 p2(1) = p1(1) + p1(3) + 10;
 set(h1,'Position',p1)
 set(h2,'Position',p2)
