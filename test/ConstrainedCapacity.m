e576350f24dea737cd92357aac904fee146ee9a5
w395650200 <dawei.zju@gmail.com>
2017-02-28 04:16:52 +0800

    use loop to simplify the test

--- test/ConstrainedCapacity.m
+++ test/ConstrainedCapacity.m
@@ -5,183 +5,59 @@
 % IEEE Transactions on Information Theory,
 % 1982, 28(1):55-67.
 %
-%% QPSK
+%% m-QAM
+function ConstrainedCapacity(bitpersym)
 
-clear
-clc
+if nargin < 1
+    bitpersym = 2;
+end
 
 RandStream.setGlobalStream(RandStream('mt19937ar','Seed',199));
 % RandStream.setGlobalStream(RandStream('mt19937ar','Seed',0));
 
-bitpersym = 2;
-mn = 2^bitpersym;
-
 symlen = 2^13;
 
-a = constellation(mn);
-
-% input power
-sp = sum(abs(a).^2)/mn;
-
-snr = 0:1:36;
-
-for pp = 1:length(snr)
-
-	th2 = 10*log10(sp) - snr(pp);
-	
-    % _TWO DIMENSION COMPLEX GAUSSIAN NOISE_
-	z = wgn(1,symlen,th2,'dbw','complex');
-	
-	for kk = 1:mn
-		for ii = 1:mn
-			tmp(ii,:) = exp(-(abs(a(kk) + z - a(ii)).^2 - abs(z).^2) / 10^(th2/10));
-		end
-		ss(kk,:) = log2(sum(tmp));
-	end
-	
-	c2 = sum(mean(ss,2));
-	
-	cc(pp) = log2(mn) - c2/mn;
-	
-end
-
-h1 = figure(1); plot(snr,cc,'k.-'); grid on; hold on;
-
-%% 8-QAM
-
-bitpersym = 3;
-mn = 2^bitpersym;
-
-a = constellation(mn);
-
-% input power
-sp = sum(abs(a).^2)/mn;
-
-snr = 0:1:36;
-
-for pp = 1:length(snr)
-
-	th2 = 10*log10(sp) - snr(pp);
-	
-    % _TWO DIMENSION COMPLEX GAUSSIAN NOISE_
-	z = wgn(1,symlen,th2,'dbw','complex');
-	
-	for kk = 1:mn
-		for ii = 1:mn
-			tmp(ii,:) = exp(-(abs(a(kk) + z - a(ii)).^2 - abs(z).^2) / 10^(th2/10));
-		end
-		ss(kk,:) = log2(sum(tmp));
-	end
-	
-	c2 = sum(mean(ss,2));
-	
-	cc(pp) = log2(mn) - c2/mn;
-	
-end
-
-h1 = figure(1); plot(snr,cc,'c.-'); grid on; hold on;
-
-%% 16-QAM
-bitpersym = 4;
-mn = 2^bitpersym;
-
-a = constellation(mn);
-
-% input power
-sp = sum(abs(a).^2)/mn;
-
-snr = 0:1:36;
-
-for pp = 1:length(snr)
-
-	th2 = 10*log10(sp) - snr(pp);
-	
-    % _TWO DIMENSION COMPLEX GAUSSIAN NOISE_
-	z = wgn(1,symlen,th2,'dbw','complex');
-	
-	for kk = 1:mn
-		for ii = 1:mn
-			tmp(ii,:) = exp(-(abs(a(kk) + z - a(ii)).^2 - abs(z).^2) / 10^(th2/10));
-		end
-		ss(kk,:) = log2(sum(tmp));
-	end
-	
-	c2 = sum(mean(ss,2));
-	
-	cc(pp) = log2(mn) - c2/mn;
-	
-end
-
-h1 = figure(1); plot(snr,cc,'g.-'); grid on;
-
-%% 32-QAM
-bitpersym = 5;
-mn = 2^bitpersym;
-
-a = constellation(mn);
-
-% input power
-sp = sum(abs(a).^2)/mn;
-
-snr = 0:1:36;
-
-for pp = 1:length(snr)
-
-	th2 = 10*log10(sp) - snr(pp);
-	
-    % _TWO DIMENSION COMPLEX GAUSSIAN NOISE_
-	z = wgn(1,symlen,th2,'dbw','complex');
-	
-	for kk = 1:mn
-		for ii = 1:mn
-			tmp(ii,:) = exp(-(abs(a(kk) + z - a(ii)).^2 - abs(z).^2) / 10^(th2/10));
-		end
-		ss(kk,:) = log2(sum(tmp));
-	end
-	
-	c2 = sum(mean(ss,2));
-	
-	cc(pp) = log2(mn) - c2/mn;
-	
-end
-
-h1 = figure(1); plot(snr,cc,'b.-'); grid on;
-
-%% 64-QAM
-bitpersym = 6;
-mn = 2^bitpersym;
-
-a = constellation(mn);
-
-% input power
-sp = sum(abs(a).^2)/mn;
-
-snr = 0:1:36;
-
-for pp = 1:length(snr)
-
-	th2 = 10*log10(sp) - snr(pp);
-	
-    % _TWO DIMENSION COMPLEX GAUSSIAN NOISE_
-	z = wgn(1,symlen,th2,'dbw','complex');
-	
-	for kk = 1:mn
-		for ii = 1:mn
-			tmp(ii,:) = exp(-(abs(a(kk) + z - a(ii)).^2 - abs(z).^2) / 10^(th2/10));
-		end
-		ss(kk,:) = log2(sum(tmp));
-	end
-	
-	c2 = sum(mean(ss,2));
-	
-	cc(pp) = log2(mn) - c2/mn;
-	
+for qq = 1:length(bitpersym)
+    mn = 2 .^ bitpersym(qq);
+    
+    % get the constellation
+    a = constellation(mn);
+    
+    % input power
+    sp = sum(abs(a).^2)/mn;
+    
+    % set snr range
+    snr = 0:1:36;
+    
+    for pp = 1:length(snr)
+        % get the power of noise
+        th2 = 10*log10(sp) - snr(pp);
+        
+        % _TWO DIMENSION COMPLEX GAUSSIAN NOISE_
+        z = wgn(1, symlen, th2, 'dbw', 'complex');
+        
+        for kk = 1:mn
+            for ii = 1:mn
+                tmp(ii,:) = exp(-(abs(a(kk) + z - a(ii)).^2 - abs(z).^2) / 10^(th2/10));
+            end
+            ss(kk,:) = log2(sum(tmp));
+        end
+        % the second term
+        c2 = sum(mean(ss,2));
+        
+        cc(pp,qq) = log2(mn) - c2/mn;
+    end
 end
 
 %% Limit for complex modulation
 lcc = log2(1 + idbw(snr));
 
-h1 = figure(1); plot(snr,cc,'r.-'); plot(snr,lcc,'k-'); grid on; ylim([1,10]);
-hold off
-xlabel('SNR (dB)'); ylabel('Capacity (bit/symbol)'); legend('QPSK','8-QAM','16-QAM','32-QAM','64-QAM');
+figure; hold on; grid on
+plot(snr,lcc,'k-'); 
+plot(snr,cc); 
+ylim([1,10]);
+xlabel('SNR (dB)'); 
+ylabel('Capacity (bit/symbol)'); 
+legend('Gaussian','QPSK','8-QAM','16-QAM','32-QAM','64-QAM');
 
+return
