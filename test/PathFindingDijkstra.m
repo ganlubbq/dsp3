e77ccacb535391af79fccf08667c50405a4845c6
w395650200 <dawei.zju@gmail.com>
2017-03-04 06:15:51 +0800

    add diagonal move and random obstacles

--- test/PathFindingDijkstra.m
+++ test/PathFindingDijkstra.m
@@ -1,5 +1,5 @@
-%% Path finding demo with the dijkstra's algortihm
-
+%% Path finding in a grid based graph with the dijkstra's algortihm
+% obstacle is realized by varying the edge weight
 function PathFindingDijkstra(N, source, destination)
 if nargin < 1
     N = 10;
@@ -11,12 +11,15 @@ if nargin < 3
     destination = [N, N];
 end
 
-tic;
+allow_diagonal_move = 1;
+allow_random_obstacle = 1;
 
 nodes = ones(N);
-tab_c = ones(N + 1);
-map_c = [1.0  1.0  1.0;
-    0.5  0.5  0.5];
+tab_c = zeros(N + 1);
+map_b = [1.0  1.0  1.0;     % white 0 - unvisited
+    0.5  0.5  0.5;          % gray  1 - visited
+    1.0  0.0  0.0;          % red   2 - final path
+    0  0  0];               % black 3 - obstacles
 
 % table to store the status of visited nodes
 tab_vstd = zeros(N);
@@ -25,22 +28,40 @@ tab_prev = cell(N);
 % table to store the current distance
 tab_dist = ones(N) * inf;
 
+% table to store the obstacles, using color code 3 to select the black
+if allow_random_obstacle
+    obstndx = randperm(N^2, 10);
+else
+    obstndx = [45, 46, 47, 55, 65, 75];
+end
+tab_vstd(obstndx) = 3;
+
 % drawing
+colormap(map_b);
+% add one more row and column
 tab_c(1:N, 1:N) = tab_vstd;
 h = pcolor(tab_c);
-colormap(map_c);
 shading faceted
 axis ij
 
 % initial
 tab_dist(source(1), source(2)) = 0;
-% edges
-offset = {[-1 0], [1 0], [0 -1], [0 1], [-1 -1], [1 -1], [-1 1], [1 1]};
-% diagonal move is slower than the other directions
-weight = [1, 1, 1, 1, 1.4, 1.4, 1.4, 1.4];
+
+% edges in grid based graph
+if allow_diagonal_move
+    offset = {[-1 0], [1 0], [0 -1], [0 1], [-1 -1], [1 -1], [-1 1], [1 1]};
+    % diagonal move is slower than the other directions
+    weight = [1, 1, 1, 1, 1.4, 1.4, 1.4, 1.4];
+    num_neighbor = 8;
+else
+    offset = {[-1 0], [1 0], [0 -1], [0 1]};
+    weight = [1, 1, 1, 1];
+    num_neighbor = 4;
+end
 
 pointer = source;
 
+tic;
 while ~ isempty(pointer)
     % marke the current node visited
     tab_vstd(pointer(1), pointer(2)) = 1;
@@ -51,14 +72,15 @@ while ~ isempty(pointer)
     axis ij
     
     % find the neighbor of current node
-    for ii = 1 : 8
+    for ii = 1 : num_neighbor
         % validate first
         neighbor = pointer + offset{ii};
-        if neighbor(1) == 0 || neighbor(2) == 0 || neighbor(1) > N || neighbor(2) > N
-            % out of map
+        if neighbor(1) == 0 || neighbor(2) == 0 || neighbor(1) > N || neighbor(2) > N || tab_vstd(neighbor(1), neighbor(2)) == 1
+            % out of map OR has been visited
         else
-            % in the map, make sure it is unvisited
-            if tab_vstd(neighbor(1), neighbor(2)) == 0
+            tempndx = sub2ind(size(nodes), neighbor(1), neighbor(2));
+            % in the map AND unvisited AND not an obstacle
+            if isempty(find(obstndx == tempndx, 1))
                 dist = tab_dist(pointer(1), pointer(2)) + weight(ii);
                 if dist < tab_dist(neighbor(1), neighbor(2))
                     tab_dist(neighbor(1), neighbor(2)) = dist;
@@ -80,8 +102,6 @@ while ~ isempty(pointer)
 end
 
 path = destination;
-tab_path = ones(N);
-
 destndx = sub2ind(size(nodes), destination(1), destination(2));
 
 while ~ isempty(tab_prev{destndx})
@@ -89,9 +109,11 @@ while ~ isempty(tab_prev{destndx})
     pointer = tab_prev{destndx};
     destndx = sub2ind(size(nodes), pointer(1), pointer(2));
     
+    % drawing, using color code 2 to select red
     intpath = sub2ind(size(nodes), path(:,1), path(:,2));
-    tab_path(intpath) = 0;
-    pcolor(~tab_path);
+    tab_vstd(intpath) = 2;
+    tab_c(1:N, 1:N) = tab_vstd;
+    pcolor(tab_c);
     axis ij
     
     pause(0.01);
