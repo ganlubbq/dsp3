ee2f9751d583920475b224c5890cb6f12cfa8022
w395650200 <dawei.zju@gmail.com>
2017-03-06 02:23:59 +0800

    complete the a star algorithm

--- test/PathFindingDijkstra.m
+++ test/PathFindingDijkstra.m
@@ -2,14 +2,18 @@
 % obstacle is realized by varying the edge weight
 %
 % Test Cases:
-%   PathFindingDijkstra(10, [1 1], [10 10], 0, 0, 0);
-%   PathFindingDijkstra(10, [1 1], [10 10], 0, 1, 0);
-%   PathFindingDijkstra(10, [1 1], [10 10], 0.1, 0, 0);
-%   PathFindingDijkstra(10, [1 1], [10 10], 0.1, 0, 1);
-%   PathFindingDijkstra(10, [1 1], [10 10], 0.1, 1, 0);
-%   PathFindingDijkstra(10, [1 1], [10 10], 0.1, 1, 1);
-%   while 1; PathFindingDijkstra(10, [1 1], [10 10], .25, 0, 1); pause(0.5); end
-%   while 1; PathFindingDijkstra(10, [1 1], [10 10], .25, 1, 1); pause(0.5); end
+%   PathFindingDijkstra(10, [1 1], [10 10], 0, 0, 0, 0);
+%   PathFindingDijkstra(10, [1 1], [10 10], 0, 0, 0, 1);
+%   PathFindingDijkstra(10, [1 1], [10 10], 0, 1, 0, 0);
+%   PathFindingDijkstra(10, [1 1], [10 10], 0, 1, 0, 1);
+%   PathFindingDijkstra(10, [1 1], [10 10], 0.1, 0, 0, 0);
+%   PathFindingDijkstra(10, [1 1], [10 10], 0.1, 0, 0, 1);
+%   PathFindingDijkstra(10, [1 1], [10 10], 0.1, 1, 0, 0);
+%   PathFindingDijkstra(10, [1 1], [10 10], 0.1, 1, 0, 1);
+%   while 1; PathFindingDijkstra(10, [1 1], [10 10], .25, 0, 1, 0); pause(0.5); end
+%   while 1; PathFindingDijkstra(10, [1 1], [10 10], .25, 0, 1, 1); pause(0.5); end
+%   while 1; PathFindingDijkstra(10, [1 1], [10 10], .25, 1, 1, 0); pause(0.5); end
+%   while 1; PathFindingDijkstra(10, [1 1], [10 10], .25, 1, 1, 1); pause(0.5); end
 %
 function [path, tab_dist] = PathFindingDijkstra(N, source, destination, obsratio, allow_diagonal_move, allow_random_obstacle, allow_heuristic)
 if nargin < 1
@@ -64,8 +68,10 @@ end
 tab_vstd = ones(N) * color_code_unvst;
 % table to store the previous node coordinates (i, j)
 tab_prev = cell(N);
-% table to store the current distance
+% table to store the current distance defined by edge
 tab_dist = ones(N) * inf;
+% table to store the current score defined by distance + heuristic
+tab_scre = ones(N) * inf;
 
 % table to store the obstacles, using color code 3 to select the black;
 % force the source and goal nodes not being obstacles
@@ -92,6 +98,7 @@ axis ij
 
 % initial
 tab_dist(source(1), source(2)) = 0;
+tab_scre(source(1), source(2)) = 0;
 
 % edges in grid based graph, if diagonal move is allowed, the wavefront is
 % round, or curved. If diagonal move is not allowed, the wavefront is flat
@@ -107,47 +114,66 @@ else
 end
 
 pointer = source;
+iteration = 0;
 
 tic;
 while ~ isempty(pointer)
     % marke the current node visited
     tab_vstd(pointer(1), pointer(2)) = color_code_vsted;
+    iteration = iteration + 1;
     
     % drawing
     tab_c(1:N, 1:N) = tab_vstd;
     pcolor(tab_c);
     axis ij
+    title(sprintf('iteration %d', iteration));
     
     % find the neighbor of current node
     for ii = 1 : num_neighbor
         % validate first
         neighbor = pointer + offset{ii};
         if neighbor(1) == 0 || neighbor(2) == 0 || neighbor(1) > N || neighbor(2) > N || tab_vstd(neighbor(1), neighbor(2)) == color_code_vsted
-            % out of map OR has been visited
-        else
-            tempndx = sub2ind(size(nodes), neighbor(1), neighbor(2));
-            % in the map AND unvisited AND not an obstacle
-            if tab_vstd(neighbor(1), neighbor(2)) == color_code_unvst
-                dist = tab_dist(pointer(1), pointer(2)) + weight(ii);
-                if dist < tab_dist(neighbor(1), neighbor(2))
-                    tab_dist(neighbor(1), neighbor(2)) = dist;
-                    tab_prev{neighbor(1), neighbor(2)} = pointer;
+            % out of map OR has been visited, skip this neighbor
+        elseif tab_vstd(neighbor(1), neighbor(2)) == color_code_unvst
+            % in the map AND unvisited AND not an obstacle, update its
+            % score accordingly
+            dist = tab_dist(pointer(1), pointer(2)) + weight(ii);
+            if dist < tab_dist(neighbor(1), neighbor(2))
+                tab_dist(neighbor(1), neighbor(2)) = dist;
+                tab_prev{neighbor(1), neighbor(2)} = pointer;
+                if allow_heuristic
+                    tab_scre(neighbor(1), neighbor(2)) = dist + heuristic(destination, neighbor);
+                else
+                    tab_scre(neighbor(1), neighbor(2)) = dist;
                 end
             end
         end
     end
     
-    % find the global min distance in unvisited set
+    % find the global min scre in unvisited set; in case multiple nodes
+    % have the same score, select one with least distance
     unvndx = find(tab_vstd == color_code_unvst);
-    [temp, mn] = min(tab_dist(unvndx));
-    % some places you just can not go
+    list_scre = tab_scre(unvndx);
+%     [temp, mn] = min(tab_scre(unvndx));
+    unvndx_minscre = unvndx(list_scre == min(list_scre));
+    list_dist = tab_dist(unvndx_minscre);
+    [temp, mn] = min(list_dist);
+    
+    % some places you simply can not go
     if isinf(temp)
         break;
     else
-        [sub1, sub2] = ind2sub(size(nodes), unvndx(mn));
+        [sub1, sub2] = ind2sub(size(nodes), unvndx_minscre(mn));
     end
     % update the current node
     pointer = [sub1, sub2];
+    
+    % if the destination is reached, terminate, otherwise the while loop
+    % will run until all nodes are visited, i.e. no solution is found;
+    % comment it out if you want to loop all the nodes.
+    if sub1 == destination(1) && sub2 == destination(2)
+        break;
+    end
     
     pause(0.01);
 end
@@ -168,8 +194,16 @@ while ~ isempty(tab_prev{pointer(1), pointer(2)})
     tab_c(1:N, 1:N) = tab_vstd;
     pcolor(tab_c);
     axis ij
+    title(sprintf('iteration %d', iteration));
     
     pause(0.01);
 end
 toc;
+return
+
+% educated guess of cost from node "neighbor" to node "destination"
+% it is highly recommended to UNDERESTIMATE the heuristic
+function h = heuristic(destination, neighbor)
+h2 = (destination(1) - neighbor(1)) .^ 2 + (destination(2) - neighbor(2)) .^ 2;
+h = 1.9 * sqrt(h2);
 return
