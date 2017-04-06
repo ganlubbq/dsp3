6beca797f86ff3ba0cce9ef6a63b9cf13fdf820b
w395650200 <dawei.zju@gmail.com>
2017-04-06 22:24:21 +0800

    first test of adadelta

--- test/PllQamSGD.m
+++ test/PllQamSGD.m
@@ -8,8 +8,11 @@
 % Kay, Steven M. "Fundamentals of statistical signal processing: estimation
 % theory." (1993).
 %
+% Test cases
+%   
+%
 % M-QAM WITH TIME VARYING PHASE ERROR
-function [] = PllQamSGD(bitpersym, snr, mu1, mu2)
+function [] = PllQamSGD(bitpersym, snr, pnvar, cfo, mu1, mu2)
 
 if nargin < 1
     bitpersym = 2;
@@ -18,16 +21,22 @@ if nargin < 2
     snr = 20;
 end
 if nargin < 3
-    mu1 = 0.01;
+    pnvar = 1e-5;
 end
 if nargin < 4
+    cfo = 1e-3;
+end
+if nargin < 5
+    mu1 = 0.01;
+end
+if nargin < 6
     mu2 = 0.001;
 end
 
 RandStream.setGlobalStream(RandStream('mt19937ar','Seed',0));
 
-symlen = 2^16;
-tvec = 0 : (1/2e9) : ((symlen-1) * (1/2e9));
+symlen = 2^14;
+tvec = 0 : 1 : (symlen-1);
 tvec = tvec(:);
 
 % input power
@@ -43,28 +52,26 @@ sigma2 = 10*log10(sp) - snr;
 z = wgn(size(symTx,1), size(symTx,2), sigma2, 'dbw', 'complex');
 
 % define phase noies
-txLaserPnVar = 2 * pi * 10e3 / 2e9;
+txLaserPnVar = 2 * pi * pnvar;
 phaseNoise = genLaserPhaseNoise(symlen, txLaserPnVar, pi/6);
 phaseNoise = phaseNoise(:);
 
 % add phase noise with cfo
-cfo = 20e6;
 symTxPn = symTx .* exp(1i*phaseNoise) .* exp(1i*2*pi*cfo*tvec);
 
 % add white gaussian noise
 symTxPn = symTxPn + z;
 
-% Solving the nonlinear LS function with gradient descent method
-% Using least squares equalization model, i.e., J = |x*exp(-j*phi)-ref|^2
-
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Solving the nonlinear least squares problem using stochastic gradient
+% descent algorithm. The cost function is J = |rx*exp(-i*phi)-tx|^2
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % initialize stochastic gradient descent algorithm, implemented as a
 % phase-lock loop, using 1 sample per symbol
-% mu1 = 0.01;         % gain parameter 1st-order
-% mu2 = 0.001 * 1;    % gain parameter 2nd-order
 symRec(1) = symTxPn(1);
 phi(1) = 0;
 nco(1) = 0;
-for k = 2:length(symTxPn)
+for k = 2 : length(symTxPn)
     % output
     symRec(k) = symTxPn(k) .* exp(-1i * phi(k-1));
     
@@ -83,12 +90,65 @@ end
 
 truePhase = unwrap(angle((symTxPn-z) .* conj(symTx)));
 
-% phi = mod(phi, 2*pi);
-% truePhase = mod(truePhase, 2*pi);
+figure; 
+subplot(221); plot(symTxPn,'.'); grid on; %axis([-2.5 2.5 -2.5 2.5]);
+subplot(222); plot(symRec,'.'); grid on; %axis([-2.5 2.5 -2.5 2.5]);
+subplot(223); plot(tvec,mod(phi,2*pi),tvec,mod(truePhase,2*pi),'r'); grid on
+subplot(224); plot(dbw(J(1:1000))); grid on; %xlim([0 symlen]); ylim([-100 20])
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Solving the nonlinear least squares problem using stochastic gradient
+% descent algorithm with adaptive step size - Adadelta
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+symRec(1) = symTxPn(1);
+phi(1) = 0;
+nco(1) = 0;
+gamma = 0.9;
+meanSquareGradient = 0;
+meanSquareDeltaPhi = 0;
+deltaPhi = 0.0;
+for k = 2 : length(symTxPn)
+    % output
+    symRec(k) = symTxPn(k) .* exp(-1i * phi(k-1));
+    
+    % stochastic gradient, also a PED with a typical S-curve
+    grad(k) = -imag(symRec(k) .* conj(symTx(k)));
+    
+    % update mean square of gradient
+    meanSquareGradient = gamma * meanSquareGradient + (1 - gamma) * grad(k)^2;
+    
+    % update mean square of delta phi
+    meanSquareDeltaPhi = gamma * meanSquareDeltaPhi + (1 - gamma) * deltaPhi^2;
+    
+    % rms of gradient
+    rmsGradient = sqrt(meanSquareGradient + 1e-8);
+    
+    % rms of delta phi
+    rmsDeltaPhi = sqrt(meanSquareDeltaPhi + 1e-8);
+    
+    % err integration
+    nco(k) = nco(k-1) + grad(k);
+    
+    % 
+    deltaPhi = (mu1 / rmsGradient) * grad(k);
+    
+    % update filter coeff. along opposite direction of gradient
+    phi(k) = phi(k-1) - deltaPhi - mu2 * nco(k);
+    
+    % squared error
+    J(k) = abs(symRec(k) - symTx(k)) .^ 2;
+end
+
+truePhase = unwrap(angle((symTxPn-z) .* conj(symTx)));
 
 figure; 
 subplot(221); plot(symTxPn,'.'); grid on; %axis([-2.5 2.5 -2.5 2.5]);
 subplot(222); plot(symRec,'.'); grid on; %axis([-2.5 2.5 -2.5 2.5]);
-subplot(223); plot(tvec,phi,tvec,truePhase,'r'); grid on
+subplot(223); plot(tvec,mod(phi,2*pi),tvec,mod(truePhase,2*pi),'r'); grid on
 subplot(224); plot(dbw(J(1:1000))); grid on; %xlim([0 symlen]); ylim([-100 20])
+
+
+
+
+
 return
