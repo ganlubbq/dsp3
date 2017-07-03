7387eef6f1169659b50382dda04ecfe1624a9fa0
w395650200 <dawei.zju@gmail.com>
2017-07-04 04:54:14 +0800

    move files

--- lib/calcJonesVector.m
+++ lib/calcJonesVector.m
@@ -15,10 +15,10 @@ function jv = calcJonesVector(azi, ell)
 % Copyright 2015 Default
 
 while abs(azi) > 90
-    azi = (abs(azi)-180) * sign(azi);
+    azi = (abs(azi) - 180) * sign(azi);
 end
 while abs(ell) > 45
-    ell = (abs(ell)-90) * sign(ell);
+    ell = (abs(ell) - 90) * sign(ell);
 end
 
 % convert to radius
@@ -26,20 +26,20 @@ ita = azi / 180 * pi;
 eps = ell / 180 * pi;
 
 % get the power split ratio
-k = (1 - cos(2*ita)*cos(2*eps)) / 2;
+k = (1 - cos(2*ita) * cos(2*eps)) / 2;
 
 % get the phase difference
 if k == 0 || k == 1
     d = 0;
 else
-    d = asin(sin(2*eps) / 2 / sqrt(k*(1-k)));
+    d = asin(sin(2*eps) / 2 / sqrt(k * (1 - k)));
 end
 
-jv = [sqrt(1-k); sqrt(k) * exp(1j*d)];
+jv = [sqrt(1 - k); sqrt(k) * exp(1i * d)];
 
 % if it is in Western Hemisphere %
 if ita < 0
-    jv = [sqrt(1-k); -sqrt(k) * exp(1j*-d)];
+    jv = [sqrt(1 - k); -sqrt(k) * exp(1i * -d)];
 end
 
 return
