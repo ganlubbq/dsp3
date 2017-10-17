9a77b478292e84e622d4f09a2da7ea17e84adea0
w395650200 <dawei.zju@gmail.com>
2017-10-18 05:00:49 +0800

    add new files...

---
+++ project/Technote/estimation/LMS_RLS_ChannelCharacterization.m
@@ -0,0 +1,56 @@
+%% Chracterize (estimate) the channel impulse response by sending known 
+% sequence and using LMS and RLS filters.
+
+clear
+
+nsample = 2000; % sample size
+
+s = 2 * (randi([0 1], nsample, 1) - 0.5);
+
+h = [0.01, 0.2, 0.25, 0.5, 1.0];
+h = h ./ sum(h);
+
+%c1 = conv(s, h, 'same');
+%c2 = fftfilt(h, s);
+c3 = filter(h, 1, s);
+
+x = zeros(size(c3));
+p = length(h);
+for ii = 1 : nsample - p
+    ss = s((1:p) + (ii-1));
+    x(ii) = fliplr(h) * ss;
+end
+x = x(:) * 10;
+
+sigma2 = .1; % noise power
+w = gaussian_noise(nsample, 1, sigma2, 'linear', 'real');
+
+x = x + w;
+
+
+% estimation initialization - lms
+mu = 0.01;
+he_lms = zeros(p, 1);
+
+for ii = 1 : nsample - p
+    ss = s((1:p) + (ii-1));
+    he_lms = he_lms - mu * (transpose(he_lms) * ss - x(ii)) * conj(ss);
+    err_lms(ii) = transpose(he_lms) * ss - x(ii);
+end
+
+% estimation initialization - rls
+he_rls = zeros(p, 1);
+Sigma = 100 * eye(p); % covariance matrix of estimation
+
+for ii = 1 : nsample - p
+    ss = s((1:p) + (ii-1));
+    gain = Sigma * ss / (1 + transpose(ss) * Sigma * ss);
+    he_rls = he_rls + gain * (x(ii) - transpose(ss) * he_rls);
+    err_rls(ii) = x(ii) - transpose(ss) * he_rls;
+end
+
+figure;
+subplot(211); plot(err_lms); grid on; xlabel('samples'); ylabel('error');
+legend('LMS');
+subplot(212); plot(err_rls); grid on; xlabel('samples'); ylabel('error');
+legend('RLS');
---
+++ project/Technote/estimation/LMS_RLS_MeanWGN.m
@@ -0,0 +1,49 @@
+%% Estimate the mean of WGN using LMS filter and RLS filter. The two filters
+% can be considered as stochastic version of gradien descent and Gauss
+% method, respectively.
+
+% The mean of WGN is assumed to be deterministic and hence this is classic
+% estimation.
+
+% As expected, RLS filter converges much faster than LMS filter at expense
+% of high computation complexity.
+
+clear
+
+A = 10; % the true mean
+nsample = 2000; % sample size
+
+sigma2 = 1; % noise power
+w = gaussian_noise(nsample, 1, sigma2, 'linear', 'real');
+
+x = A + w;
+
+
+% estimation initialization
+mu = 0.01;
+xe_lms = zeros(size(x));
+
+gain = zeros(size(x)); % gain
+xe_rls = zeros(size(x));
+variance = zeros(size(x)); % variance of estimation
+
+for ii = 1 : nsample
+    if ii == 1
+        xe_lms(ii) = x(1);
+        
+        xe_rls(ii) = x(1);
+        variance(ii) = sigma2;
+    else
+        xe_lms(ii) = xe_lms(ii-1) + mu * (x(ii) - xe_lms(ii-1));
+        
+        gain(ii) = variance(ii-1) / (variance(ii-1) + 1);
+        xe_rls(ii) = xe_rls(ii-1) + gain(ii) * (x(ii) - xe_rls(ii-1));
+        variance(ii) = (1 - gain(ii)) * variance(ii-1);
+    end
+end
+
+figure;
+subplot(311); plot(dbw(variance)); grid on; xlabel('samples'); ylabel('variance');
+subplot(312); plot(dbw(gain)); grid on; xlabel('samples'); ylabel('gain');
+subplot(313); plot(dbw(xe_rls)); grid on; hold on; plot(xe_lms); 
+xlabel('samples'); ylabel('estimated mean'); legend('RLS', 'LMS');
